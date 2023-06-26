98256e4e0052d3a46875af23fc93933f896d87ac
paulzolotov <zolotov10pl@gmail.com>
2023-06-26 10:49:26 +0300

    Initial project

---
+++ coffee_shop/goldstar_coffee/goldstar_coffee/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for goldstar_coffee project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'goldstar_coffee.settings')
+
+application = get_asgi_application()
