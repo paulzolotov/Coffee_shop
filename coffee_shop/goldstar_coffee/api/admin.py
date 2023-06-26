300b5d3e16cebe2c816e1ccf1567d79b15a1d489
paulzolotov <zolotov10pl@gmail.com>
2023-06-26 17:00:47 +0300

    addes drinks models

--- coffee_shop/goldstar_coffee/api/admin.py
+++ coffee_shop/goldstar_coffee/api/admin.py
@@ -1,3 +1,105 @@
 from django.contrib import admin
+from django.http import HttpRequest
+from django.urls import reverse
+from django.utils.html import format_html
+from django.utils.http import urlencode
+
+from .models import Category, Drinks, VarietyInDrinkCategory
 
 # Register your models here.
+
+
+class FunctionsForActions(admin.ModelAdmin):
+    """Класс для повторяющихся функций для всех классов отображения"""
+
+    @admin.action(description="Перевести в неактивное состояние")
+    def make_inactive(self, request: HttpRequest, queryset) -> None:
+        """Функция для перевода в строке 'action' в состояние 'inactive'"""
+
+        queryset.update(is_active=False)
+
+    @admin.action(description="Перевести в активное состояние")
+    def make_active(self, request: HttpRequest, queryset) -> None:
+        """Функция для перевода в строке 'action' в состояние 'active'"""
+
+        queryset.update(is_active=True)
+
+
+@admin.register(Category)
+class CategoryAdmin(FunctionsForActions):
+    """Класс для отображения на панели администратора информации о категории напитка."""
+
+    list_display = (
+        "name",
+        "is_active",
+        "slug",
+        "view_drinks_link"
+    )
+
+    actions = ("make_inactive", "make_active")
+
+    @admin.display(description="drinks")
+    def view_drinks_link(self, obj):
+        """Функция для подсчета количества напитков определенной категории, а также генерации ссылки
+        на эти напитки"""
+
+        count = obj.drinks_set.count()
+        url = (
+            reverse("admin:api_drinks_changelist")
+            + "?"
+            + urlencode({"departure_category_id": f"{obj.id}"})
+        )
+        return format_html('<a href="{}">{} Drinks</a>', url, count)
+
+
+@admin.register(VarietyInDrinkCategory)
+class VarietyInDrinkCategoryAdmin(FunctionsForActions):
+    """Класс для отображения на панели администратора информации о разнообразии определенного напитка в категории."""
+
+    list_display = (
+        "name",
+        "is_active",
+        "slug",
+        "view_drinks_link"
+    )
+
+    actions = ("make_inactive", "make_active")
+
+    @admin.display(description="drinks")
+    def view_drinks_link(self, obj):
+        """Функция для подсчета количества напитков определенной категории, а также генерации ссылки
+        на эти напитки"""
+
+        count = obj.drinks_set.count()
+        url = (
+            reverse("admin:api_drinks_changelist")
+            + "?"
+            + urlencode({"departure_category_of_sort_drink_id": f"{obj.id}"})
+        )
+        return format_html('<a href="{}">{} Drinks</a>', url, count)
+
+
+@admin.register(Drinks)
+class DrinksAdmin(FunctionsForActions):
+    """Класс для отображения на панели администратора информации о напитках."""
+
+    list_display = (
+        "name",
+        "is_active",
+        "slug",
+        "image",
+        "category",
+        "category_of_sort_drink",
+        "price",
+        "description",
+    )
+
+    sortable_by = ("name", "price", "category", "category_of_sort_drink")
+    list_filter = ("name", "price", "category", "category_of_sort_drink")
+    actions = ("make_inactive", "make_active")
+
+    @admin.display(description="custom price")
+    def show_pretty_price(self, obj) -> str:
+        """Функция для отображения кастомной записи для 'цены за место' (В данном случае добавили знак $)"""
+
+        return f"{obj.price} BYN"
--- coffee_shop/goldstar_coffee/api/models.py
+++ coffee_shop/goldstar_coffee/api/models.py
@@ -1,3 +1,77 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+
 
 # Create your models here.
+class ShopInfoMixin(models.Model):
+    """Класс Mixin, для повторяющихся полей, от которого затем наследуются классы с моделями"""
+
+    slug = models.SlugField(max_length=70, verbose_name="Short Name")
+    name = models.CharField(max_length=70, verbose_name="Name")
+    is_active = models.BooleanField(default=True, verbose_name="Is it active?")
+
+    class Meta:
+        abstract = True
+
+
+class Category(ShopInfoMixin):
+    """Класс для создания модели - Категория Напитка"""
+
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
+    def __str__(self) -> str:
+        """Возвращает удобочитаемую строку для каждого объекта."""
+
+        return f"{self.name}"
+
+
+class VarietyInDrinkCategory(ShopInfoMixin):
+    """Класс для создания модели - Разновидность кофе/чая/ ... """
+
+    class Meta:
+        verbose_name = "Variety"
+        verbose_name_plural = "Varieties"
+
+    def __str__(self) -> str:
+        """Возвращает удобочитаемую строку для каждого объекта."""
+
+        return f"{self.name}"
+
+
+class Drinks(ShopInfoMixin):
+    """Класс для создания модели - Напитки"""
+
+    CHOICES = (
+        ("1", "S"),
+        ("2", "M"),
+        ("3", "L"),
+    )
+
+    category = models.ForeignKey(
+        Category, verbose_name="Drink Category", on_delete=models.CASCADE
+    )
+    category_of_sort_drink = models.ForeignKey(
+        VarietyInDrinkCategory, verbose_name="Variety In Drink Category", on_delete=models.CASCADE
+    )
+    description = models.TextField(max_length=500, verbose_name="description")
+    price = models.IntegerField(
+        default=0, validators=[MinValueValidator(1)], verbose_name="Price per drink"
+    )
+    image = models.FileField(upload_to="api/")
+    # glass_size = models.CharField(
+    #     max_length=10, choices=CHOICES, verbose_name="Glass size"
+    # )
+    # rating = models.IntegerField(
+    #     default=0, validators=[MinValueValidator(1), MaxValueValidator(5)], verbose_name="Drink rating from user"
+    # )
+
+    class Meta:
+        verbose_name = "Drinks"
+        verbose_name_plural = "Drinks"
+
+    def __str__(self) -> str:
+        """Возвращает удобочитаемую строку для каждого объекта."""
+
+        return f"{self.name}"
